2ec83ce232d5c776141830f747b5933ed9dfd6ff
lpast48 <ukaszpasternak@gmail.com>
2017-10-28 20:26:16 +0200

    hello word

---
+++ library-management-system/library-management-system/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_management_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello word");
+            Console.Read();
+        }
+    }
+}
